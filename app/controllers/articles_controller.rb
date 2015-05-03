f086836c04a9d9e11d864c75da3af5495c75654d
田所 駿佑 <korosuke@allfesta.com>
2015-05-03 20:49:46 +0900

    チュートリアル終わり

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,7 @@
 class ArticlesController < ApplicationController
 
+  http_basic_authenticate_with name: "dhh", password: "secret", except: [:index, :show]
+
   def index
     @articles = Article.all
   end
@@ -21,8 +23,35 @@ class ArticlesController < ApplicationController
     #renderによる描画は、フォームの送信時と同じリクエスト内で行われます。対照的に、redirect_toはサーバーに別途リクエストを発行するようブラウザに対して指示するので、やりとりが1往復増えます。
   end
 
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      p "== article updated ===================="
+      p "@article"
+      pp @article
+      p "article_params"
+      pp article_params
+      p "======================================="
+      redirect_to @article
+    else
+      render 'edit'
+    end
+  end
+
   def show
     @article = Article.find(params[:id])
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+
+    redirect_to articles_path
   end
 
   private
